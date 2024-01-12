c572c9681cfb05436ff9ebf19c6ade567deffcd3
Subashri24 <subashrisk04@gmail.com>
2024-01-12 18:53:30 +0530

    Second changes by user_a

---
+++ src/Demo/Swapping_of_two_numbers.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Swapping_of_two_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Swapping of two numbers");
+	}
+
+}
